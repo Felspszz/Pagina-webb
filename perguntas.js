be88d3f29b15699c916d2746b00a18db88c343a8
Felspszz <felipe.ribeiro09@escola.pr.gov.br>
2025-05-14 09:35:46 -0300

    14/05/2025

--- perguntas.js
+++ perguntas.js
@@ -11,13 +11,13 @@ criaCartao(
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'História',
+    'Em que período da história japonesa a classe samurai começou a se consolidar?',
+    'No período Heian (794-1185).'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Samurais',
+    'Qual era a arma principal de um samurai?',
+    'A katana, uma espada longa, curva e de fio único, era considerada a "alma do samurai".'
 )
